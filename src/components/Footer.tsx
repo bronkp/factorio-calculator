7cff569e4a126b57fed7f50aaacf621888cbe57a
rory <rorysaxton@gmail.com>
2023-03-29 16:45:29 -0700

    changed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer:React.FC = () => {
 <Flex width='100%' bgColor='orange.400' p={5} position='fixed' bottom={0}  justifyContent="center">
     <VStack mt='auto'>
     <Text >Welcome to my calculator! </Text>
-    <Text>This project was mainly for practice in React, so it may not have all the features you're looking for.</Text>
+    <Text>This project was mainly for practice in React, so it may not have all the features you&apos;re looking for.</Text>
     <Text >Contact me: rorysaxton@gmail.com</Text>
     <Link href='https://github.com/bronkp'>Github</Link>
 

f93cc040c07ff5034162f442dd73a3c23851f58d
rory <rorysaxton@gmail.com>
2023-03-29 16:06:40 -0700

    Removed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,15 +5,7 @@ const Footer:React.FC = () => {
 
 
     return (
-<Flex width='100%' bgColor='orange.400' p={5} position='fixed' bottom={0}  justifyContent="center">
-    
-    <Text >Welcome to my calculator! </Text>
-    <Text>This project was mainly for practice in React, so it does not have everything.</Text>
-    <Text >Contact me: rorysaxton@gmail.com</Text>
-   
-
-    
-    </Flex>
+<Text>a</Text>
     )
 
 }

ee33642661874530021cfd61d16a2a877316695b
rory <rorysaxton@gmail.com>
2023-03-29 16:03:28 -0700

    removing margin auto from footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,13 +6,13 @@ const Footer:React.FC = () => {
 
     return (
 <Flex width='100%' bgColor='orange.400' p={5} position='fixed' bottom={0}  justifyContent="center">
-    <VStack mt='auto'>
+    
     <Text >Welcome to my calculator! </Text>
     <Text>This project was mainly for practice in React, so it does not have everything.</Text>
     <Text >Contact me: rorysaxton@gmail.com</Text>
    
 
-    </VStack>
+    
     </Flex>
     )
 
